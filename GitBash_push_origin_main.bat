173ff63c3954fef9e76007e5d3de2a0debb2c437
Madaimnim <madaimnim@gmail.com>
2025-03-13 00:14:36 +0800

    更新專案

--- GitBash_push_origin_main.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d D:\Practice\TowerDefenseGame
-git add .
-git commit -m "更新專案"
-git push origin main
-exit
---
+++ upload_to_github.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /d/Practice/TowerDefenseGame
+git add .
+git commit -m "更新專案"
+git push origin main
+read -p "按任意鍵繼續..."
